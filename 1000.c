410541a3aa45fbdfa4fb38621281e5e8f98e0aa8
Arthur-Uehara <arthur.uehara@unifesp.br>
2026-04-29 23:11:18 -0300

    Update and rename uUAuau to 1000.c

---
+++ 1000.c
@@ -0,0 +1,18 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Linguagem   : C
+Nome        : Arthur Hideaki Uehara
+Plataforma  : https://judge.beecrowd.com/pt/search
+Exercício   : 1000
+Data        : 29/04/2026
+Objetivo    : Escrever hello world!
+Aprendizado : Usra os comandos básicos da linguagem c
+-------------------------------------------------------------------------- */
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World!");
+
+    return 0;
+}
